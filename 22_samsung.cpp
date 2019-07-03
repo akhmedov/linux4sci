be449023f4087ac41bd91e337492cd531f353e09
Rolan Akhmedov <rolan.kharkiv@gmail.com>
2019-07-03 14:13:49 +0300

    22: samsung1 dynamic approach

--- 22_samsung.cpp
+++ 22_samsung.cpp
@@ -1,49 +1,40 @@
+// https://github.com/algosolver01/algorithms/tree/master/Отрезки
+
 #include <iostream>
-#define CANDIDATES 1000000
-#define MAX(a,b) (((a)>(b))?(a):(b))
+using namespace std;
 
-struct Vertex { int l1{}, l2{}; };
+#define MAXN 50
+#define MAXSUM 1000
 
-Vertex prev[CANDIDATES], next[CANDIDATES];
+int K[MAXN], RUNS, N;
+bool DP[MAXSUM+1][MAXN+1];
 
-int split (int* K, int N)
+int subset_sum (int* K, int N)
 {
-    if (N < 2) return 0;
-    int max = 0, total = 2;
+    for (int j = 0; j <= N; j++) DP[0][j] = true;
+    for (int i = 1; i <= MAXSUM; i++) DP[i][0] = false;
 
-    prev[0].l1 = K[0]; prev[0].l2 = 0;
-    prev[1].l1 = prev[1].l2 = 0;
-
-    for (int i = 1; i < N; i++, total *= 3) {
-        for (int j = 0, j3 = 0; j < total; j++, j3 = 3*j) {
-
-            next[j3] =  prev[j];
-            next[j3].l1 += K[i];
-            if (next[j3].l1 == next[j3].l2) max = MAX(max,next[j3].l1);
-
-            next[j3+1] =  prev[j];
-            next[j3+1].l2 += K[i];
-            if (next[j3+1].l1 == next[j3+1].l2) max = MAX(max,next[j3+1].l1);
-
-            next[j3+2] = prev[j];
+    for (int j = 1; j <= N; j++) {
+        for (int i = 1; i <= MAXSUM; i++) {
+            if (DP[i][j-1]) DP[i][j] = true;
+            else if (K[j-1] > i) DP[i][j] = false;
+            else DP[i][j] = DP[i-K[j-1]][j-1];
         }
-        for (int j = 0; j < 3*total; j++) prev[j] = next[j];
     }
 
-    return max;
+    for (int i = MAXSUM; i > 0; i--) if (DP[i][N] && DP[i/2][N]) return i/2;
+    return 0;
 }
 
 int main ()
 {
     freopen("22_input.txt", "r", stdin);
-
-    int RUNS, K[50], N;
-    std::cin >> RUNS;
+    cin >> RUNS;
 
     for (int i = 0; i < RUNS; i++) {
-        std::cin >> N;
-        for (int j = 0; j < N; j++) std::cin >> K[j];
-        std::cout << split(K, N) << std::flush << std::endl;
+        cin >> N;
+        for (int j = 0; j < N; j++) cin >> K[j];
+        cout << subset_sum(K, N) << endl;
     }
 
     return 0;
